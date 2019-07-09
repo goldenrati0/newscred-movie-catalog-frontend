cc7a20f214501ed76c4cf0f6b65c78af5dd8f8cd
Tahmid Choyon <tahmid.choyon@gmail.com>
2019-07-09 23:34:39 +0600

    :bento: Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, Switch} from "react-router-dom";
+import HomePage from "./components/pages/homepage";
+import FavoritePage from "./components/pages/favoritepage";
+import UserLoginPage from "./components/pages/user-login";
+import UserSignupPage from "./components/pages/signup-page";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Switch>
+            <Route exact path={"/"} component={HomePage}/>
+            <Route exact path={"/home"} component={HomePage}/>
+            <Route exact path={"/favorite"} component={FavoritePage}/>
+            <Route exact path={"/login"} component={UserLoginPage}/>
+            <Route exact path={"/signup"} component={UserSignupPage}/>
+        </Switch>
+    );
 }
 
 export default App;
